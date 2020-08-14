a6dfb58b831a8ed6c2284bd8c5a5a18f7d487a3e
Bruno Bacelar Abe <messengerabe@hotmail.com>
2020-08-13 21:50:40 -0300

    Cleaning code
    Retirando código legado

--- domain/lib/model/favorite_movie.dart
+++
@@ -1,9 +0,0 @@
-import 'package:meta/meta.dart';
-
-class FavoriteMovie {
-  const FavoriteMovie({
-    @required this.posterUrl,
-  }) : assert(posterUrl != null);
-
-  final String posterUrl;
-}
--- domain/lib/model/production_country.dart
+++
@@ -1,8 +0,0 @@
-import 'package:meta/meta.dart';
-
-class ProductionCountry {
-  const ProductionCountry({
-    @required this.name,
-  }) : assert(name != null);
-  final String name;
-}
